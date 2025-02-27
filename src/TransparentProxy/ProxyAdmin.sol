26ff45aa0f891854677ad2ed9799b8757fa0c420
Nejc Schneider <nejc.sch@gmail.com>
2025-02-27 23:58:03 +0100

    feat: add ProxyAdmin.sol

---
+++ src/TransparentProxy/ProxyAdmin.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./TransparentProxy.sol"; // Import TransparentProxy contract
+
+contract ProxyAdmin is Ownable {
+    // Function to upgrade the proxy to a new implementation
+    function upgrade(
+        address proxy,
+        address newImplementation
+    ) external onlyOwner {
+        TransparentProxy(payable(proxy)).upgradeTo(newImplementation);
+    }
+}
