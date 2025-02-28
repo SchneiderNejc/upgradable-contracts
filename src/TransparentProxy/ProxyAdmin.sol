57e5c2204a1dae6d3834878ca2abe3399113e840
Nejc Schneider <nejc.sch@gmail.com>
2025-02-28 23:21:44 +0100

    feat: add upgradeProxy()

--- src/TransparentProxy/ProxyAdmin.sol
+++ src/TransparentProxy/ProxyAdmin.sol
@@ -5,12 +5,16 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./TransparentProxy.sol"; // Import TransparentProxy contract
 
 contract ProxyAdmin is Ownable {
-    // Function to upgrade the proxy to a new implementation
-    function upgrade(
     constructor(address initialOwner) Ownable(initialOwner) {}
+
+    function upgradeProxy(
         address proxy,
-        address newImplementation
-    ) external onlyOwner {
-        TransparentProxy(payable(proxy)).upgradeTo(newImplementation);
+        address newImplementation,
+        bytes memory data
+    ) public onlyOwner {
+        TransparentProxy(payable(proxy)).upgradeToAndCall(
+            newImplementation,
+            data
+        );
     }
 }
